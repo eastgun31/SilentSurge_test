ce4ca8d42b97dce0af320abf76c2a68b04abcf0a
junseok1014 <gkswnstjr8523@gmail.com>
2023-09-11 15:06:52 +0900

    0911한준석 카메라
    카메라 가속, 카메라 범위 제한

--- Assets/HJS/Scripts/CameraController.cs
+++ Assets/HJS/Scripts/CameraController.cs
@@ -4,27 +4,33 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float camSpeed = 30f;
+    public float camSpeed = 50f;
 
     public float scrollSpeed = 50f;
 
     public float minY = 5f;
-    public float maxY = 25f;
-    
+    public float maxY = 35f;
+
+    float minX = 0;
+    float maxX = 230;
+    float minZ = 0;
+    float maxZ = 230;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
+        FastCam();
         Vector3 pos = transform.position;
 
         //À§
-        if(Input.GetKey(KeyCode.W)) 
+        if (Input.GetKey(KeyCode.W))
         {
             pos.z += camSpeed * Time.deltaTime;
         }
@@ -45,10 +51,26 @@ public class CameraController : MonoBehaviour
         }
 
         float scroll = Input.GetAxis("Mouse ScrollWheel");
-        pos.y -= scroll * scrollSpeed * 100f * Time.deltaTime;
-    
+        pos.y -= scroll * scrollSpeed * 10f * Time.deltaTime;
+
         pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+        pos.x = Mathf.Clamp(pos.x, minX, maxX);
+        pos.z = Mathf.Clamp(pos.z, minZ, maxZ);
 
         transform.position = pos;
     }
+
+    void FastCam()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            camSpeed = 100;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            camSpeed = 50;
+        }
+    }
+
 }
