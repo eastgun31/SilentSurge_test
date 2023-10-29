e80732b6c64dc2e336429f004c76f8f83e72d645
junseok1014 <gkswnstjr8523@gmail.com>
2023-10-29 20:08:01 +0900

    1029한준석
    로비 씬 정리
    마우스 이벤트 코드 수정

--- Assets/Scripts/Ui/MouseEvent.cs
+++ Assets/Scripts/Ui/MouseEvent.cs
@@ -1,45 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
+
+//public class MouseEvent : MonoBehaviour
+//{
+//    public GameObject image_Change;
+//    public bool im_check = true;
+//    Audio_Manager audio_Manager;
+
+//    void Start()
+//    {
+//        audio_Manager = FindAnyObjectByType<Audio_Manager>();
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+        
+//    }
+
+//    private void OnMouseEnter()
+//    {
+//        audio_Manager.PlaySFX(audio_Manager.AudioButton);
+//        if (im_check = true)
+//        {
+//            image_Change.SetActive(true);
+//        }
+//        else
+//        {
+//        }
+//    }
+
+//    private void OnMouseExit()
+//    {
+//        if (im_check = true)
+//        {
+//            image_Change.SetActive(false);
+//        }
+//        else
+//        {
+//        }
+//    }
+//}
 
 public class MouseEvent : MonoBehaviour
 {
     public GameObject image_Change;
-    public bool im_check = true;
-    Audio_Manager audio_Manager;
-
-    void Start()
-    {
-        audio_Manager = FindAnyObjectByType<Audio_Manager>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Audio_Manager audio_Manager;
 
     private void OnMouseEnter()
     {
         audio_Manager.PlaySFX(audio_Manager.AudioButton);
-        if (im_check = true)
-        {
-            image_Change.SetActive(true);
-        }
-        else
-        {
-        }
+        image_Change.SetActive(true);
     }
 
     private void OnMouseExit()
     {
-        if (im_check = true)
-        {
-            image_Change.SetActive(false);
-        }
-        else
-        {
-        }
+        image_Change.SetActive(false);
     }
 }
