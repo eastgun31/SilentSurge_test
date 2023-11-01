05ff04064347216b12578e4acad99c2f10924575
leekijung00 <143581427+leekijung00@users.noreply.github.com>
2023-11-01 16:48:11 +0900

    11-01 how to play 설명추가 타임스케일 추가

---
+++ Assets/Scripts/Ui/Time_timeScale.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Time_timeScale : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
